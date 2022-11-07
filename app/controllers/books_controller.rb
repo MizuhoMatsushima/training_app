0d7cd66e1af29f3f011c5b1dcd93b53579fa2093
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-11-07 05:32:13 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -12,17 +12,17 @@ class BooksController < ApplicationController
       render index
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_index_path
   end
-  
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, middle_of_tags_attributes:[:id, :tag_id])
   end
-  
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   has_many :tags, through: :middle_of_tags
   has_many :middle_of_tags, dependent: :destroy
+  accepts_nested_attributes_for :middle_of_tags, allow_destroy: true
 end
